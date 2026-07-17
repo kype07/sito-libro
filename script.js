bd0ceb714ad37ef1e438227162b8b2c74f167a6c
nicolo' sciuto <148966962+kype07@users.noreply.github.com>
2026-07-17 19:24:13 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,28 @@ function scaricaPDF() {
     link.download = "USS Valera   Ex Astris, Scientia (ultimo).pdf";
     link.click();
 }
+const starsContainer = document.getElementById("stars");
+
+// crea 150 stelle
+for (let i = 0; i < 150; i++) {
+    let star = document.createElement("div");
+    star.className = "star";
+
+    star.style.top = Math.random() * 100 + "%";
+    star.style.left = Math.random() * 100 + "%";
+
+    star.style.animation = `twinkle ${Math.random() * 3 + 2}s infinite`;
+
+    starsContainer.appendChild(star);
+}
+
+// animazione CSS dinamica
+const style = document.createElement("style");
+style.innerHTML = `
+@keyframes twinkle {
+    0% { opacity: 0.2; }
+    50% { opacity: 1; }
+    100% { opacity: 0.2; }
+}
+`;
+document.head.appendChild(style);
